a214738b94661a284ce292c36935e51c85c79efa
lulislaw <artyg7lul@gmail.com>
2022-08-05 07:41:44 +0300

    Initial commit

---
+++ app/src/main/java/com/example/dule2/DBHelper.java
@@ -0,0 +1,44 @@
+package com.example.dule2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Build;
+
+import androidx.annotation.Nullable;
+
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "duledb";
+    public static final String TABLE_CONTACTS = "contacts";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_WEEK = "week";
+    public static final String KEY_ID = "_id";
+    public static final String KEY_TIME = "time";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
+                + " integer primary key," + KEY_NAME + " text," + KEY_TIME + " text," + KEY_WEEK + " text" + ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("drop table if exists " + TABLE_CONTACTS);
+
+        onCreate(db);
+
+
+    }
+}
+
+
+
